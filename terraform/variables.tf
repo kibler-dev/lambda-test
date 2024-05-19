ad6bab57171a7228848c41ed2c77b9758e5eb575
Kevin Kibler <kjkibler13@gmail.com>
2024-05-19 10:16:20 -0500

    try with tfc env vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "lambda_function_name" {
   description = "Name of the Lambda function"
   default     = "hello-world-lambda"
 }
+
+variable "aws_assume_role_arn" {
+  description = "ARN of the role to assume when creating resources"
+  default     = ""
+}
+
+variable "aws_web_identity_token_file" {
+  description = "OIDC token file to use when assuming the role"
+  default     = ""
+}
